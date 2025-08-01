3a625b6c53e86ccfebf2afda54c2c4cffb938824
Bienedict Caballero <bcaballero06578@liceo.edu.ph>
2025-08-01 09:54:25 +0800

    lab-activity-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int totalNumber = 0;
+        int number;
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            number = scanner.nextInt();
+            totalNumber += number;
+        }
+
+        System.out.println("\nTotal Number: " + totalNumber);
+
+        scanner.close();
     }
 }
